9268cc80a5249e48971096dbbcce0e8a23f87072
Piyush <pvpatil5643@gmail.com>
2026-04-08 10:37:55 +0530

    Experiment 1 to 6

---
+++ Exp04/Replace_Methods_with_lambda_expressions/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Replace_Methods_with_lambda_expressions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello, World!");
+        }
+    }
+}
